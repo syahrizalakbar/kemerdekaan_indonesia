92f76bd6fc415218cc36a62d8313380dad491cfb
syahrizalakbar <syahrizalakbar@gmail.com>
2019-09-04 02:52:12 +0000

    perubahan ui

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,9 @@ class Merdeka extends StatelessWidget {
 
       appBar: AppBar(
         title: Text('Kemerdekaan Indonesia'),
+        actions: <Widget>[
+          Icon(Icons.bookmark_border)
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -37,13 +40,16 @@ class Merdeka extends StatelessWidget {
               padding: EdgeInsets.all(8),
               child: Center(
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Icon(Icons.star),
-                    Icon(Icons.star),
-                    Icon(Icons.star),
-                    Icon(Icons.star),
-                    Icon(Icons.star_half),
+                    Text('45rb disukai'),
+                    Row(children: <Widget>[
+                      Icon(Icons.star),
+                      Icon(Icons.star),
+                      Icon(Icons.star),
+                      Icon(Icons.star),
+                      Icon(Icons.star_half),
+                    ],),
                   ],
                 ),
               ),
@@ -53,36 +59,35 @@ class Merdeka extends StatelessWidget {
               child: Text(
                   'Setiap tahun pada tanggal 17 Agustus, rakyat Indonesia merayakan Hari Proklamasi Kemerdekaan ini dengan meriah. Beragam perlombaan dihadirkan mulai dari lomba panjat pinang, lomba makan kerupuk, hingga upacara militer di Istana Merdeka, serta seluruh masyarakat ikut berpartisipasi dengan caranya masing-masing.'),
             ),
+            RaisedButton(
+              color: Colors.red,
+              onPressed: () {
+                print('Tertekan');
+              },
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.school),
+                  Container(
+                    padding: EdgeInsets.only(left: 4),
+                    child: Text('Pelajari'),
+                  )
+                ],
+              ),
+            ),
             Container(
               padding: EdgeInsets.all(8.0),
               color: Colors.grey[100],
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: <Widget>[
-                  menuBawah(Icons.share, 'Bagikan'),
                   menuBawah(Icons.thumb_up, 'Suka'),
                   menuBawah(Icons.pan_tool, 'Partisipasi'),
                   menuBawah(Icons.explore, 'Jelajahi'),
+                  menuBawah(Icons.share, 'Bagikan'),
                 ],
               ),
             ),
-            Center(
-              child: RaisedButton(
-                color: Colors.red,
-                onPressed: () {
-                  print('Tertekan');
-                },
-                child: Row(
-                  children: <Widget>[
-                    Icon(Icons.school),
-                    Container(
-                      padding: EdgeInsets.only(left: 4),
-                      child: Text('Pelajari'),
-                    )
-                  ],
-                ),
-              ),
-            ),
+            
           ],
         ),
       ),
